d249bb147ae05d4c6dcff0e30a0764d869697286
H Cui <hcui8000@gmail.com>
2019-08-18 11:59:01 -0700

    added checkout-item

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 import {selectCartItemCount} from '../../redux/cart/cart.selectors';
 import './cart-icon.styles.scss';
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = state => ({
-    itemCount: selectCartItemCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
